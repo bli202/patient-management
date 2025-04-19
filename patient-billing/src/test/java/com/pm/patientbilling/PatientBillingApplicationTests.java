b0607c7f8c94aea77fe4077cd783aa665f97e9db
Ben Li <109922463+bli202@users.noreply.github.com>
2025-04-19 15:42:01 -0700

    rollback

---
+++ patient-billing/src/test/java/com/pm/patientbilling/PatientBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.patientbilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientBillingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
